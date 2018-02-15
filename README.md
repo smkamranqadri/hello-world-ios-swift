a88797df2d93320b685436a155f8f8f743864783
Nano Degree <nanodegree@Nanos-Mac.local>
2018-02-15 20:23:50 +0500

    Hellow World application created

---
+++ README.md
@@ -0,0 +1,5 @@
+# Hello World
+## In iOS using Swift
+
+![](screenshots/screenshot1.png)
+![](screenshots/screenshot2.png)
--- helloWorld/ViewController.swift
+++ helloWorld/ViewController.swift
@@ -9,7 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var background: UIImageView!
 
+    @IBOutlet weak var text: UIImageView!
+    
+    @IBOutlet weak var welcomeBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +27,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func welcomeAction(_ sender: AnyObject) {
+
+            background.isHidden = false
+            text.isHidden = false
+            welcomeBtn.isHidden = true
+    }
 
 }
 
